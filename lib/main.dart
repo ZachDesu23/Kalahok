3a474dc88e5046dd4fae9a837342afd2fb7caf95
Joshua <jlagan@sprout.ph>
2023-08-14 15:26:41 +0800

    run application from ios platform

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
     statusBarColor: Colors.transparent,
   ));
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -17,12 +16,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: GetStarted(),
     );
   }
 }
-
-
-
